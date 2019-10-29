db0e38a6f6876c482b73957ef1561219a7691802
Roman Perekrestov <mr.perekrestov@gmail.com>
2019-10-29 16:03:06 +0100

    ready for test

--- LogCalculator.ConsoleApp/Program.cs
+++ LogCalculator.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace LogCalculator.ConsoleApp
 {
@@ -6,7 +8,53 @@ namespace LogCalculator.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var result = Calculate(new TimeSpan(0, 0, 10), new TimeSpan(0, 0, 1));
+            if (File.Exists("result.csv"))
+            {
+                File.Delete("result.csv");
+            }
+            result.ForEach(result =>
+            {
+                File.AppendAllText("result.csv",$"{Math.Round(result.timePassed.TotalSeconds, MidpointRounding.ToZero)}, {result.currentValue}\n");
+            });              
         }
+        static List<LogCalculationResult> Calculate(TimeSpan period, TimeSpan logginPeriod)
+        {
+            var result = new List<LogCalculationResult>();
+            var startTime = DateTime.UtcNow;
+            var lastLogged = DateTime.UtcNow;
+            long i = 1;
+            while (DateTime.UtcNow.Subtract(startTime) < period)
+            {
+                unchecked
+                {
+                    Math.Log10(i);
+                    Math.Pow(i, 2);
+                    Math.Sqrt(i);
+                    Math.Exp(i);
+                }
+                i++;
+                Log();
+            }
+            void Log()
+            {
+                if (DateTime.UtcNow.Subtract(lastLogged) > logginPeriod)
+                {
+                    result.Add(new LogCalculationResult
+                    {
+                        currentValue = i,
+                        timePassed = DateTime.UtcNow.Subtract(startTime)
+                    });
+                    lastLogged = DateTime.UtcNow;
+                }
+
+            }
+            return result;
+        }
+    }
+    class LogCalculationResult
+    {
+        public long currentValue { get; set; }
+        public TimeSpan timePassed { get; set; }
     }
 }
